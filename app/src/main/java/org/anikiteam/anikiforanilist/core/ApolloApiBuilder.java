27508f527f17f02fd98233d106ead78a8d1f4bbc
Michael Ai <michael.ai@outlook.com>
2018-07-22 15:24:36 +0200

    Commented out Apollo example.

--- app/src/main/java/org/anikiteam/anikiforanilist/core/ApolloApiBuilder.java
+++ app/src/main/java/org/anikiteam/anikiforanilist/core/ApolloApiBuilder.java
@@ -1,18 +1,11 @@
 package org.anikiteam.anikiforanilist.core;
 
 import com.apollographql.apollo.ApolloClient;
-import com.apollographql.apollo.api.Operation;
-import com.apollographql.apollo.api.ResponseField;
-import com.apollographql.apollo.cache.normalized.CacheKey;
-import com.apollographql.apollo.cache.normalized.CacheKeyResolver;
 import com.apollographql.apollo.cache.normalized.NormalizedCacheFactory;
 import com.apollographql.apollo.cache.normalized.lru.EvictionPolicy;
 import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCacheFactory;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Collections;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.ConnectionSpec;
@@ -62,35 +55,36 @@ public class ApolloApiBuilder {
         return ApolloClient.builder()
                 .serverUrl(baseUrl)
                 .okHttpClient(okHttpClient())
-                .normalizedCache(normalizedCacheFactory(), cacheKeyResolver())
+                .normalizedCache(normalizedCacheFactory())
                 .build();
     }
 
-    private CacheKeyResolver cacheKeyResolver(){
-        return new CacheKeyResolver() {
-            @NotNull
-            @Override
-            public CacheKey fromFieldRecordSet(@NotNull ResponseField field, @NotNull Map<String, Object> recordSet) {
-                String typeName = (String) recordSet.get("__typename");
-                if ("User".equals(typeName)) {
-                    String userKey = typeName + "." + recordSet.get("login");
-                    return CacheKey.from(userKey);
-                }
-                if (recordSet.containsKey("id")) {
-                    String typeNameAndIDKey = recordSet.get("__typename") + "." + recordSet.get("id");
-                    return CacheKey.from(typeNameAndIDKey);
-                }
-                return CacheKey.NO_KEY;
-            }
-
-            // Use this resolver to customize the key for fields with variables: eg entry(repoFullName: $repoFullName).
-            // This is useful if you want to make query to be able to resolved, even if it has never been run before.
-            @NotNull @Override
-            public CacheKey fromFieldArguments(@NotNull ResponseField field, @NotNull Operation.Variables variables) {
-                return CacheKey.NO_KEY;
-            }
-        };
-    }
+    //todo consider functionality
+//    private CacheKeyResolver cacheKeyResolver(){
+//        return new CacheKeyResolver() {
+//            @NotNull
+//            @Override
+//            public CacheKey fromFieldRecordSet(@NotNull ResponseField field, @NotNull Map<String, Object> recordSet) {
+//                String typeName = (String) recordSet.get("__typename");
+//                if ("User".equals(typeName)) {
+//                    String userKey = typeName + "." + recordSet.get("login");
+//                    return CacheKey.from(userKey);
+//                }
+//                if (recordSet.containsKey("id")) {
+//                    String typeNameAndIDKey = recordSet.get("__typename") + "." + recordSet.get("id");
+//                    return CacheKey.from(typeNameAndIDKey);
+//                }
+//                return CacheKey.NO_KEY;
+//            }
+//
+//            // Use this resolver to customize the key for fields with variables: eg entry(repoFullName: $repoFullName).
+//            // This is useful if you want to make query to be able to resolved, even if it has never been run before.
+//            @NotNull @Override
+//            public CacheKey fromFieldArguments(@NotNull ResponseField field, @NotNull Operation.Variables variables) {
+//                return CacheKey.NO_KEY;
+//            }
+//        };
+//    }
 
     private OkHttpClient.Builder okHttpClientBuilder() {
         OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
